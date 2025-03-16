ff794d95a344d6aacdd0d6f89711ec0349558fb1
Horashi <horashidev@gmail.com>
2025-03-17 03:08:23 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
-
+  const myFunction = () => {
+    console.log('>> run my Function')
+  }
+  myFunction()
   return (
     <>
       <div>
@@ -16,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello World & Horashidev
+      <h1>Hello World & Horashi dev
       </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
