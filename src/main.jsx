511a8ce6382e77ee46c605213b82e4cca102d705
Horashi <horashidev@gmail.com>
2025-03-30 12:43:31 +0700

    video 52

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,17 @@ import {
 import LoginPage from './pages/login.jsx';
 import RegisterPage from './pages/register.jsx';
 import UserPage from './pages/user.jsx';
-import ProductPage from './pages/product.jsx';
+import BookPage from './pages/book.jsx';
 import './style/global.css'
 import TodoApp from './components/todo/TodoApp.jsx';
+import ErrorPage from './pages/error.jsx';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
+    errorElement: <ErrorPage/>,
     children: [
       {
         index: true,
@@ -28,7 +30,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/products",
-        element: <ProductPage/>
+        element: <BookPage/>
       }
     ]
   },
