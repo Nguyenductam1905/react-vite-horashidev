5dfd8a036a63f1c500529e9220388ff98688f23f
Horashi <horashidev@gmail.com>
2025-03-30 12:28:31 +0700

    video 51

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import TodoNew from './components/todo/TodoNew'
-import TodoData from './components/todo/TodoData'
 import Header from './layout/header'
 import Footer from './layout/footer'
 import { Outlet, } from 'react-router-dom'
 
-const App = () => {
-  const horashiDev = "DucTam HorashiDev";
-  const age = 25;
-  const data = {
-    address: "HaNoi",
-    country: "VietNam"
-  }
-  // const addNewTodo = (name) => {
-  //   alert(`call me ${name}`)
-  // }
-  const [todoList, setTodoList] = useState([
-    // { id: 1, name: "Leaning React" },
-    // { id: 2, name: "Watching Youtube" }
-  ])
-  const addNewTodo = (name) => {
-    const newTodo = {
-      id:randomIntFromInterval(3, 1000),
-      name:name
-    }
-    setTodoList([...todoList, newTodo])
-  }
-  const deleteTasks = (taskDelete) => {
-    setTodoList(todoList.filter((list) => list.id !== taskDelete))
-  }
-  const randomIntFromInterval = (min, max) => { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
 
+const App = () => {
   return (
     <>
         <Header/>
-        <div className="todo-container">
-          <div className='todo-title'>Todo List</div>
-          <TodoNew
-           addNewTodo = {addNewTodo}
-          />
-          {/* {
-            todoList.length > 0 &&
-            <TodoData
-            // name = {horashiDev}
-            // age = {age}
-            // data = {data}
-            todoList = {todoList}
-          />
-          }
-          {todoList.length === 0 &&
-            <div className="todo-image">
-              <img src={reactLogo} alt="" />
-            </div>
-          } */}
-          {
-            todoList.length > 0 ?
-            <TodoData
-            // name = {horashiDev}
-            // age = {age}
-            // data = {data}
-            todoList = {todoList}
-            deleteTasks = {deleteTasks}
-          />
-          :
-          <div className="todo-image">
-              <img src={reactLogo} alt="" />
-            </div>
-          }
-        </div>
         <Outlet/>
         <Footer/>
     </>
---
+++ src/components/todo/TodoApp.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+import reactLogo from '../../assets/react.svg'
+import './todo.css'
+import TodoNew from './TodoNew'
+import TodoData from './TodoData'
+
+const TodoApp = () => {
+    const horashiDev = "DucTam HorashiDev";
+    const age = 25;
+    const data = {
+      address: "HaNoi",
+      country: "VietNam"
+    }
+    // const addNewTodo = (name) => {
+    //   alert(`call me ${name}`)
+    // }
+    const [todoList, setTodoList] = useState([
+      // { id: 1, name: "Leaning React" },
+      // { id: 2, name: "Watching Youtube" }
+    ])
+    const addNewTodo = (name) => {
+      const newTodo = {
+        id:randomIntFromInterval(3, 1000),
+        name:name
+      }
+      setTodoList([...todoList, newTodo])
+    }
+    const deleteTasks = (taskDelete) => {
+      setTodoList(todoList.filter((list) => list.id !== taskDelete))
+    }
+    const randomIntFromInterval = (min, max) => { // min and max included 
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+  
+    return(
+      <div className="todo-container">
+            <div className='todo-title'>Todo List</div>
+            <TodoNew
+             addNewTodo = {addNewTodo}
+            />
+            {/* {
+              todoList.length > 0 &&
+              <TodoData
+              // name = {horashiDev}
+              // age = {age}
+              // data = {data}
+              todoList = {todoList}
+            />
+            }
+            {todoList.length === 0 &&
+              <div className="todo-image">
+                <img src={reactLogo} alt="" />
+              </div>
+            } */}
+            {
+              todoList.length > 0 ?
+              <TodoData
+              // name = {horashiDev}
+              // age = {age}
+              // data = {data}
+              todoList = {todoList}
+              deleteTasks = {deleteTasks}
+            />
+            :
+            <div className="todo-image">
+                <img src={reactLogo} alt="" />
+              </div>
+            }
+          </div>
+    )
+  }
+
+  export default TodoApp
